3fd57810629df1dfedc89f83c5fc69ec085af14f
Nicholas Speedy <nspeedy@F3C024.42adel.org.au>
2021-12-14 17:38:59 +1030

    go

--- fdf/includes/fdf.h
+++ fdf/includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: nspeedy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 17:38:47 by nspeedy           #+#    #+#             */
-/*   Updated: 2021/12/13 15:23:15 by nspeedy          ###   ########.fr       */
+/*   Updated: 2021/12/14 17:38:20 by nspeedy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FDF_H
@@ -14,5 +14,32 @@
 
 # include "mlx.h"
 # include <stdlib.h>
+
+typedef struct s_image {
+	void	*img;
+	char	*addr;
+	int		width;
+	int		height;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_image;
+
+typedef struct s_mlx {
+	void	*mlx;
+	void	*win;
+}	t_mlx;
+
+typedef struct s_pixel {
+	int	x;
+	int	y;
+	int	colour;
+}	t_pixel;
+
+typedef struct s_vector {
+	int	x;
+	int	y;
+	int	z;
+}	t_vector;
 
 #endif
--- fdf/srcs/open_window.c
+++ fdf/srcs/open_window.c
@@ -6,39 +6,11 @@
 /*   By: nspeedy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 18:20:24 by nspeedy           #+#    #+#             */
-/*   Updated: 2021/12/13 15:34:39 by nspeedy          ###   ########.fr       */
+/*   Updated: 2021/12/14 17:20:02 by nspeedy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-typedef struct s_image
-{
-	void	*img;
-	char	*addr;
-	int		width;
-	int		height;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_image;
-
-typedef struct s_mlx {
-	void	*mlx;
-	void	*win;
-}	t_mlx;
-
-typedef struct s_pixel {
-	int	x;
-	int	y;
-	int	colour;
-}	t_pixel;
-
-typedef struct s_vector {
-	int x;
-	int y;
-	int z;
-}	t_vector;
 
 void	put_pixel(t_image *img, int x, int y, int colour)
 {
@@ -98,9 +70,6 @@ void	put_quad(t_image *img, int x, int y, int w, int h, int c)
 		}
 		i++;
 	}
-	/*for (int i = x; i < x + (w/2); i++)
-		for (int j = y; j < y + (h/2); j++)
-			put_pixel(img, i, j, c);*/
 }
 
 int	main(void)
