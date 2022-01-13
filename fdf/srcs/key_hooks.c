d27cc0f99e13dc8049408a765cc2c48a12288ea9
Nicholas Speedy <nspeedy@F3C022.42adel.org.au>
2022-01-13 13:11:38 +1030

    should be a working parse, needs main

---
+++ fdf/srcs/key_hooks.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_hooks.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nspeedy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/06 16:53:34 by nspeedy           #+#    #+#             */
+/*   Updated: 2022/01/06 17:15:18 by nspeedy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	close(void)
+{
+	exit(0);
+	return (0);
+}
+
+int	keypress(int key, void *data)
+{
+	data = 0;
+	if (key == 53)
+		close();
+	return (0);
+}
+
+void	mlx_window(void *win)
+{
+	mlx_hook(win, 2, 1L << 0, keypress, 0);
+	mlx_hook(win, 17, 1L << 17, close, 0);
+}
